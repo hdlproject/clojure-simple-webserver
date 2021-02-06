34049e9ccaf396c44f2100d6c5018264a68dc44e
Hendra Danu Laksana <hdl.project.co@gmail.com>
2021-02-06 22:40:35 +0700

    feat(config): update config key

--- src/clojure_simple_webserver/config.clj
+++ src/clojure_simple_webserver/config.clj
@@ -3,7 +3,7 @@
 (defn get-config []
   {
    :port                    (or (System/getenv "PORT") "8080")
-   :covid19_service_address (System/getenv "COVID19_SERVICE_ADDRESS")
+   :covid19-service-address (System/getenv "COVID19_SERVICE_ADDRESS")
    })
 
 (def config (get-config))
